391d1cbb2594a43ec118bd5fa9e129f4dd679382
syabyr <mybays@sina.com>
2023-11-09 00:23:58 +0800

    fix

--- markdown/get_weekly.py
+++ markdown/get_weekly.py
@@ -106,56 +106,96 @@ def gen_md(raw,path):
     # 正文内容
     body = json.loads(script[0].text)['props']['pageProps']['cp2Content']
 
+    mdFile.write('\n\n')
+
     if('body' not in body):
-        print(body)
-        return -1
-
-
-    for item in body['body']:
-        if(item['type'] == 'CROSSHEAD'):
-            mdFile.new_paragraph(item['text'],bold_italics_code='bi', color='purple')
-        elif(item['type'] == 'PARAGRAPH'):
-            # 根据个人喜好,选择是否使用html格式
-            mdFile.new_paragraph(item['textHtml'])
-        elif(item['type'] == 'IMAGE'):
-            # download image
-            os.makedirs(path+'/images', exist_ok=True)
-            urllib.request.urlretrieve(item['url'], path+'/images/'+item['url'].split('/')[-1])
-            picname = item['url'].split('/')[-1]
-            mdFile.write('\n![image](images/'+picname+')')
-        elif(item['type'] == 'INFOGRAPHIC'):
-            # download image
-            #print(item['fallback']['url'])
-            if(item['fallback']['url'] == None):
-                # https://www.economist.com/europe/2023/10/12/our-european-economic-pentathlon
-                continue
+        #print('body:',body)
+        text = data['text']
+        for item in text:
+            #print('item:',item)
+            children = item['children']
+            for child in children:
+                #print('child:',child)
+                if(child['type'] == 'text'):
+                    mdFile.write(child['data'])
+                    mdFile.write('\n')
+                if(child['type'] == 'tag'):
+                    attrs = child['attribs']
+                    cchildren = child['children']
+                    #print('cchildren:',cchildren)
+                    for cchild in cchildren:
+                        #print('cchild:',cchild)
+                        if('href' in attrs):
+                            if('data' in cchild):
+                                writeData = cchild['data']
+                            elif('children' in cchild):
+                                writeData = cchild['children'][0]['data']
+                            mdFile.write(mdFile.new_inline_link(link=attrs['href'], text=writeData))
+                        else:
+                            if('attribs' in cchild):
+                                aattrs = cchild['attribs']
+                                ccchildren = cchild['children']
+                                for ccchild in ccchildren:
+                                    #print('ccchild:',ccchild)
+                                    if('data' in ccchild):
+                                        writeData = ccchild['data']
+                                    elif('children' in ccchild):
+                                        writeData = ccchild['children'][0]['data']
+                                    if('href' in aattrs):
+                                        mdFile.write(mdFile.new_inline_link(link=aattrs['href'], text=writeData))
+                                    else:
+                                        mdFile.write(text=writeData)
+                            else:
+                                mdFile.write(text=cchild['data'])
+                #mdFile.write('\n\n')
+            mdFile.write('\n\n')
+    else:
+        for item in body['body']:
+            if(item['type'] == 'CROSSHEAD'):
+                mdFile.new_paragraph(item['text'],bold_italics_code='bi', color='purple')
+            elif(item['type'] == 'PARAGRAPH'):
+                # 根据个人喜好,选择是否使用html格式
+                mdFile.new_paragraph(item['textHtml'])
+            elif(item['type'] == 'IMAGE'):
+                # download image
+                os.makedirs(path+'/images', exist_ok=True)
+                urllib.request.urlretrieve(item['url'], path+'/images/'+item['url'].split('/')[-1])
+                picname = item['url'].split('/')[-1]
+                mdFile.write('\n![image](images/'+picname+')')
+            elif(item['type'] == 'INFOGRAPHIC'):
+                # download image
+                print(item);
+                print(item['fallback']['url'])
+                if(item['fallback']['url'] == None):
+                    # https://www.economist.com/europe/2023/10/12/our-european-economic-pentathlon
+                    continue
+                else:
+                    os.makedirs(path+'images', exist_ok=True)
+                    urllib.request.urlretrieve(item['fallback']['url'], path+'images/'+item['fallback']['url'].split('/')[-1])
+                    mdFile.write('\n![image]('+'images/'+item['fallback']['url'].split('/')[-1]+')')
+            elif(item['type'] == 'BOOK_INFO'):
+                mdFile.new_paragraph(item['textHtml'])
+                #https://www.economist.com/the-economist-reads/2023/11/03/six-books-you-didnt-know-were-propaganda
+                #https://www.economist.com/culture/2023/11/02/hong-kongs-year-of-protest-now-feels-like-a-mirage
+            elif(item['type'] == 'INFOBOX'):
+                components = item['components']
+                #print(item)
+                for component in components:
+                    if(component['type'] == 'PARAGRAPH'):
+                        mdFile.new_paragraph(component['textHtml'])
+                    elif(component['type'] == 'UNORDERED_LIST'):
+                        innerItems = component['items']
+                        for innerItem in innerItems:
+                            mdFile.new_paragraph(innerItem['textHtml'])
+            elif(item['type'] == 'VIDEO'):
+                #https://www.economist.com/europe/2023/10/29/trenches-and-tech-on-ukraines-southern-front
+                pass
             else:
-                os.makedirs(path+'images', exist_ok=True)
-                urllib.request.urlretrieve(item['fallback']['url'], path+'images/'+item['fallback']['url'].split('/')[-1])
-                mdFile.write('\n![image]('+'images/'+item['fallback']['url'].split('/')[-1]+')')
-        elif(item['type'] == 'BOOK_INFO'):
-            mdFile.new_paragraph(item['textHtml'])
-            #https://www.economist.com/the-economist-reads/2023/11/03/six-books-you-didnt-know-were-propaganda
-            #https://www.economist.com/culture/2023/11/02/hong-kongs-year-of-protest-now-feels-like-a-mirage
-        elif(item['type'] == 'INFOBOX'):
-            components = item['components']
-            #print(item)
-            for component in components:
-                if(component['type'] == 'PARAGRAPH'):
-                    mdFile.new_paragraph(component['textHtml'])
-                elif(component['type'] == 'UNORDERED_LIST'):
-                    innerItems = component['items']
-                    for innerItem in innerItems:
-                        mdFile.new_paragraph(innerItem['textHtml'])
-        elif(item['type'] == 'VIDEO'):
-            #https://www.economist.com/europe/2023/10/29/trenches-and-tech-on-ukraines-southern-front
-            pass
-        else:
-            print(item)
-            #mdFile.new_line(mdFile.new_reference_image(text='aaa',path=item['url'], reference_tag='im'))
-            #print(item)
-            #mdFile.new_paragraph(item['textHtml'])
-            #mdFile.new_paragraph(item['text'])
+                print(item)
+                #mdFile.new_line(mdFile.new_reference_image(text='aaa',path=item['url'], reference_tag='im'))
+                #print(item)
+                #mdFile.new_paragraph(item['textHtml'])
+                #mdFile.new_paragraph(item['text'])
 
     mdFile.create_md_file()
 

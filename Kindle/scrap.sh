7f2bd1e8ea3f07651f779ee9c8b6484d4f6a97ae
syabyr <mybays@sina.com>
2024-02-20 23:02:24 +0800

    fix

--- Kindle/scrap.sh
+++ Kindle/scrap.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 year=$(echo "$1" | cut -d "-" -f 1)
 mkdir -p "$year"
-if [ ! -f "The Economist $2.mobi" ];then
+if [ ! -f "TheEconomist-$1-$2.mobi" ];then
 
-        sed  "s/edition_date = .*/edition_date = '$1'/" economist.recipe > "The Economist $2.recipe"
+        sed  "s/edition_date = .*/edition_date = '$1'/" economist.recipe > "TheEconomist-$1-$2.recipe"
 
-        ebook-convert "The Economist $2.recipe" .mobi --output-profile=kindle_pw3 --pubdate="$1" -vv --mobi-file-type=new --title="The Economist $2"
+        ebook-convert "TheEconomist-$1-$2.recipe" .mobi --output-profile=kindle_oasis --pubdate="$1" -vv --mobi-file-type=new --authors="TheEconomist" --title="TheEconomist-$1-$2"
 
-        rm "The Economist $2.recipe"
-        cp "The Economist $2.mobi" "$year"
+        rm "TheEconomist-$1-$2.recipe"
+        mv "TheEconomist-$1-$2.mobi" "$year"
 fi

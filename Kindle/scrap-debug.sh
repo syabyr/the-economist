820cd7804ac073988acb797e765fb3ae53cc4ca5
syabyr <mybays@sina.com>
2025-11-17 21:13:02 +0800

    fix

---
+++ Kindle/scrap-debug.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+year=$(echo "$1" | cut -d "-" -f 1)
+mkdir -p "$year"
+if [ ! -f "TheEconomist-$1-$2.mobi" ];then
+
+        sed  "s/edition_date = .*/edition_date = '$1'/" economist.recipe > "TheEconomist-$1-$2.recipe"
+
+        ebook-convert "TheEconomist-$1-$2.recipe" .mobi --output-profile=kindle_oasis --pubdate="$1" -d debug -vv --mobi-file-type=new --authors="TheEconomist" --title="TheEconomist-$1-$2"
+
+        rm "TheEconomist-$1-$2.recipe"
+        mv "TheEconomist-$1-$2.mobi" "$year"
+fi
